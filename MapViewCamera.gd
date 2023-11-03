ac9c28f3559747bb162acb385bbc708403477fb8
Alex Noir <Varsash@gmail.com>
2023-11-04 00:20:25 +0300

    Add mouse scroll zoom in/zoom out Make camera movement adjusted according to zoom level

--- MapViewCamera.gd
+++ MapViewCamera.gd
@@ -1,6 +1,11 @@
 extends Camera2D
 
 func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			zoom += Vector2(0.1, 0.1)
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			zoom -= Vector2(0.1, 0.1)
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-			position -= event.relative
+			position -= (event.relative / zoom.x)
