98ac4e851f67be71f2d926103fa411f211c674e3
Alex Noir <Varsash@gmail.com>
2023-11-05 04:31:37 +0300

    Analyze the assets folder for char.inis Implement char.ini showname=charfolder associations Add parsed colors if speaker is hostname or normal Only use speaker icon in IC speech Fix scrolling events being passed to map view when logs are open Make parsed log multithreaded for speed

--- main_window.gd
+++ main_window.gd
@@ -12,6 +12,11 @@ var current_file: FileAccess
 var current_file_path: String
 var last_date_modified
 
+var hostname: String
+
+
+#{"showname": "charfolder"}
+var shownames: Dictionary = {}
 
 func _process(delta):
 	if current_file_path and last_date_modified != FileAccess.get_modified_time(current_file_path):
@@ -21,8 +26,35 @@ func _process(delta):
 			parse_logfile()
 
 
+func generate_shownames():
+	shownames.clear()
+	var dir = DirAccess.open(asset_folder_path + "/characters")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				var ini_path = dir.get_current_dir() + "/" + file_name + "/char.ini"
+				if FileAccess.file_exists(ini_path):
+					var config = FileAccess.open(ini_path, FileAccess.READ)
+					print("Found .ini file in folder: " + file_name)
+					var in_options = false
+					while not config.eof_reached():
+						var line = config.get_line()
+						if line.begins_with("["):
+							in_options = line.strip_edges().to_lower() == "[options]"
+						if in_options:
+							var split = line.split("=")
+							if split.size() > 1 and split[0].strip_edges() == "showname":
+								var showname = split[1].strip_edges()
+								if showname and showname != file_name:
+									shownames[showname] = file_name
+									print("Showname '" + showname + "' now associated with charfolder " + file_name)
+			file_name = dir.get_next()
+
+
 func parse_line(line):
-	if not line.begins_with("["):
+	if not line.begins_with("[") or not line.contains("GMT]"):
 		parsed_view.add_text(line + "\n")
 		return
 	var is_ooc = false
@@ -30,32 +62,49 @@ func parse_line(line):
 		is_ooc = true
 		line = line.substr("[OOC]".length())
 	var timestamp = line.substr(0, line.find("]"))
-	var message = line.substr(timestamp.length()+1).strip_edges()
-	var speaker = message.substr(0, message.find(":")).strip_edges()
+	line = line.substr(timestamp.length()+1)
+	var message = line.substr(line.find(":")+1).strip_edges()
+	var speaker = line.substr(0, line.find(":")).strip_edges()
 	var italics = false
-	if speaker.ends_with(" shouts"):
-		italics = true
-		speaker = speaker.substr(0, speaker.length() - " shouts".length())
-	if speaker.ends_with(" has played a song"):
-		italics = true
-		speaker = speaker.substr(0, speaker.length() - " has played a song".length())
-	var showname = speaker.substr(0, message.find("(")).strip_edges()
-	if showname != speaker:
-		speaker = speaker.substr(showname.length()+1).strip_edges().trim_prefix("(").trim_suffix(")")
-	# TODO: parse speaker even in "shouts", "plays music" etc. IC logs
-	var speaker_icon = asset_folder_path + "/characters/" + speaker + "/char_icon.png"
-#	if speaker == "$":
-#		speaker_icon = asset_folder_path + "/evidence/empty.png"
-	if FileAccess.file_exists(speaker_icon):
-		# TODO: cache this, ideally by using Godot's resource system properly
-		var image = Image.load_from_file(speaker_icon)
-		var texture = ImageTexture.create_from_image(image)
-		# Add image to the bbcode tag stack
-		parsed_view.add_image(texture, 24, 24)
+	if is_ooc:
+		if not hostname and (message.begins_with("===") or message.begins_with("Character changed")) and speaker != "[Global log]":
+			hostname = speaker
 	else:
-		push_warning("Couldn't find char_icon for %s" % [speaker_icon])
+		var actions = ["shouts", "has "]
+		for action in actions:
+			if speaker.find(action) > -1:
+				italics = true
+				speaker = speaker.substr(0, speaker.find(action))
+				break
+
+		var showname = speaker.substr(0, speaker.find("(")).strip_edges()
+		var charfolder = showname
+		if showname != speaker:
+			charfolder = speaker.substr(showname.length()+1).strip_edges().trim_prefix("(").trim_suffix(")")
+
+		if charfolder in shownames:
+			charfolder = shownames[charfolder]
+		if charfolder:
+			var speaker_icon = asset_folder_path + "/characters/" + charfolder + "/char_icon.png"
+			if FileAccess.file_exists(speaker_icon):
+				# TODO: cache this, ideally by using Godot's resource system properly
+				var image = Image.load_from_file(speaker_icon)
+				var texture = ImageTexture.create_from_image(image)
+				# Add image to the bbcode tag stack
+				parsed_view.add_image(texture, 24, 24)
+			else:
+				push_warning("Couldn't find char_icon for %s (%s)" % [speaker_icon, message])
 	if italics:
 		parsed_view.push_italics()
+	if speaker == hostname:
+		parsed_view.push_color(Color.DEEP_SKY_BLUE)
+	else:
+		parsed_view.push_color(Color.AQUAMARINE)
+	# Colored name
+	parsed_view.add_text(speaker)
+	# pop color
+	parsed_view.pop()
+	parsed_view.add_text(": ")
 	parsed_view.add_text(message + "\n")
 	if italics:
 		parsed_view.pop()
@@ -63,6 +112,7 @@ func parse_line(line):
 
 func parse_logfile():
 	parsed_view.clear()
+	hostname = ""
 	var lines = logview.text.split("\n")
 	for line in lines:
 		parse_line(line)
@@ -101,6 +151,7 @@ func _on_file_dialog_file_selected(path):
 func _on_folder_dialog_dir_selected(dir):
 	asset_folder_path = dir
 	$AssetsLabel.text = "Current base: " + asset_folder_path
+	generate_shownames()
 
 
 func _on_link_assets_button_pressed():
