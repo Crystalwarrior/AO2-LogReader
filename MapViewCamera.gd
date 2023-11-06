33d185ea5761c2e2d7a442aed81c6d5ad8b6b96b
Alex Noir <Varsash@gmail.com>
2023-11-06 23:57:55 +0300

    Make zooming more consistent

--- MapViewCamera.gd
+++ MapViewCamera.gd
@@ -2,13 +2,15 @@ extends Camera2D
 
 signal zoomChange(value)
 
+var zoom_speed = 0.1
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			zoom += Vector2(0.1, 0.1)
+			zoom += zoom * zoom_speed
 			emit_signal("zoomChange", zoom.x)
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			zoom -= Vector2(0.1, 0.1)
+			zoom -= zoom * zoom_speed
 			emit_signal("zoomChange", zoom.x)
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
